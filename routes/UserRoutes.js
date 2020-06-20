13702e7a8271e68d9d43b367e643a6c6cbca3df1
Tbonator <thabo2g5@yahoo.ca>
2020-06-20 02:27:00 +0200

    Changing routes names

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
 
-router.get("/getusers", UserController.getUsers);
-router.get("/getuser/:userId", UserController.getUser);
-router.post("/createuser", UserController.createUser);
-router.delete("/deleteuser/:userId", UserController.deleteUser);
-router.patch("/updateuser/:userId", UserController.UpdateUser);
+router.post("/users", UserController.createUser);
+router.get("/users", UserController.getUsers);
+router.get("/users/:userId", UserController.getUser);
+router.delete("/users/:userId", UserController.deleteUser);
+router.patch("/users/:userId", UserController.UpdateUser);
 
 module.exports = router;
